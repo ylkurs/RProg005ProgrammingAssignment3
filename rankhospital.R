96d1c14dd43879f4adff1a252ef6f5bbcbf0767a
Yi Li <lepetitforskare@gmail.com>
2014-07-24 16:44:23 -0700

    Add file rankhospital.R.

---
+++ rankhospital.R
@@ -0,0 +1,55 @@
+rankhospital <- function(state, outcome, num = "best") {
+	## Check state
+	if ( !(state %in% state.abb) ) {
+		stop("invalid state")
+	}
+
+	## Check outcome
+	diseases = c("heart attack", "heart failure", "pneumonia")
+	if ( !(outcome %in% diseases) ) {
+		stop("invalid outcome")
+	}
+
+	## Check num
+	ranking = 0
+	if (num == "best") {
+		ranking = 1
+	}
+	else if (num == "worst") {
+		ranking = -1
+	}
+	else if ((num %% 1 == 0) & (num > 0)) {
+		ranking = num
+	}
+	else {
+		stop("invalid ranking")
+	}
+	stop("stop")
+	## Read outcome data
+	odata <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+	hnames <- odata[, "Hospital.Name"]
+	states <- odata[, "State"]
+	harate <- as.numeric(odata[, "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"])
+	hfrate <- as.numeric(odata[, "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"])
+	pnrate <- as.numeric(odata[, "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"])
+
+	## Check that state and outcome are valid
+	sis <- which(states == state)
+	state_hnames <- hnames[sis]
+	state_rate <- vector()
+	if (outcome == "heart attack") {
+		state_rate <- harate[sis]
+	}
+	else if (outcome == "heart failure") {
+		state_rate <- hfrate[sis]
+	}
+	else {
+		state_rate <- pnrate[sis]
+	}
+	
+	## Return hospital name in that state with the given rank
+	## 30-day death rate
+	mi1 <- which( state_rate == min(state_rate, na.rm = TRUE) )
+	best_hname = min(state_hnames[mi1])
+	best_hname
+}
