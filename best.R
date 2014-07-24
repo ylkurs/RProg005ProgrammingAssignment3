2bb6912322decdc83e69fa14913f11fa51b58e7f
Yi Li <lepetitforskare@gmail.com>
2014-07-24 12:12:48 -0700

    Find the hospital name.

--- best.R
+++ best.R
@@ -13,13 +13,27 @@ best <- function(state, outcome) {
 	## Read outcome data
 	odata <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
 	hnames <- odata[, "Hospital.Name"]
+	states <- odata[, "State"]
 	harate <- as.numeric(odata[, "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"])
 	hfrate <- as.numeric(odata[, "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"])
 	pnrate <- as.numeric(odata[, "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"])
 
 	## Check that state and outcome are valid
-
-
-	## Return hospital name in that state with lowest 30-day death
-	## rate
+	sis <- which(states == state)
+	state_hnames <- hnames[sis]
+	state_rate <- vector()
+	if (outcome == "heart attack") {
+		state_rate <- harate[sis]
+	}
+	else if (outcome == "heart failure") {
+		state_rate <- hfrate[sis]
+	}
+	else {
+		state_rate <- pnrate[sis]
+	}
+	
+	## Return hospital name in that state with lowest 30-day death rate
+	mi1 <- which( state_rate == min(state_rate, na.rm = TRUE) )
+	best_hname = min(state_hnames[mi1])
+	best_hname
 }
